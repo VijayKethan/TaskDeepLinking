ee4505c4b0d96973c6be9fabe868235d052a294d
VijayKethan <neonlights.dream@gmail.com>
2017-11-06 15:58:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vcarve_dt_14/task_deeplinking/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.vcarve_dt_14.task_deeplinking;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+    }
+}
